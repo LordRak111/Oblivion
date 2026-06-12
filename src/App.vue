0e0c46fdabfc3d97d295b4a61221a9ba674611d0
Egorius52 <smittom70@gmail.com>
2026-06-12 16:26:21 +0300

    fix(app): connect projects page to hash navigation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
-
 <template>
-    <div class="app">
+  <div>
     <SiteHeader />
-    <ProductsPage />
+
+    <HomePage v-if="currentPage === 'home'" />
+    <ProductPage v-if="currentPage === 'products'" />
+    <ProjectsPage v-if="currentPage === 'projects'" />
+
     <SiteFooter />
   </div>
 </template>
 
 <script setup>
-import SiteHeader from './components/SiteHeader.vue';
-import ProductsPage from './components/ProductPage.vue';
+import { ref, onMounted } from 'vue'
+
+import SiteHeader from './components/SiteHeader.vue'
+import SiteFooter from './components/SiteFooter.vue'
+import ProductPage from './components/ProductPage.vue'
 import HomePage from './pages/HomePage.vue'
 import ProjectsPage from './pages/ProjectsPage.vue'
-import SiteFooter from './components/SiteFooter.vue'
 
+const currentPage = ref('home')
 
+const updatePage = () => {
+  const hash = window.location.hash.replace('#', '')
+
+  if (hash === 'products') {
+    currentPage.value = 'products'
+  } else if (hash === 'projects') {
+    currentPage.value = 'projects'
+  } else {
+    currentPage.value = 'home'
+  }
+}
+
+onMounted(() => {
+  updatePage()
+  window.addEventListener('hashchange', updatePage)
+})
 </script>
-
-
